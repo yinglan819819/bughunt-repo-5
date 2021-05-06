6c2a9e1c7d5b61a77113f466912cc857e44b4056
Tai Lai <tlaiamz@amazon.com>
2021-05-06 10:39:20 -0700

    Replace equality operator with equals()

--- src/main/java/com/mycompany/app/Server1111.java
+++ src/main/java/com/mycompany/app/Server1111.java
@@ -15,7 +15,7 @@ public class Server
        String s = "aloha";
        System.out.println(String.format("This is a faulty message: %i", s));
        System.out.format("No %s",1);
-       if ("1" == "2") {
+       if ("1".equals("2")) {
             //dosomething
        }
     }
